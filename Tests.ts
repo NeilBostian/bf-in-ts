137a7f882782bd786ab7f8f35d4da91e9dd8daa0
Neil Bostian <neil.bostian@gmail.com>
2022-02-08 04:25:10 -0500

    fix: test

--- Tests.ts
+++ Tests.ts
@@ -4,7 +4,7 @@ import {
     RunToEnd
 } from './Interpreter'
 
-type Ttest1 = RunToEnd<BfProgram<'+>++>+++'>>
+type Ttest1 = RunToEnd<BfProgram<'+>++>+++', ''>>
 const test1: Ttest1['mem'] = '\u0003'
 
 type Tres1 = ProgramSummary<Ttest1>
