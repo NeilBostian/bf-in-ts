beb0239230344d4c4e4c154daed7ef06c0141880
Neil Bostian <neil.bostian@gmail.com>
2022-02-08 04:24:45 -0500

    feat: refactor & basic support for stdio

--- Interpreter.ts
+++ Interpreter.ts
@@ -24,6 +24,35 @@ interface Program {
     memLeft: string
     mem: MemByte | ''
     memRight: string
+
+    stdin: string
+    stdout: string
+}
+
+interface CompletedProgram extends Program {
+    halt: true
+}
+
+export type BfProgram<
+    TBfCode extends string,
+    TStdin extends string
+> = {
+    halt: false
+    instrLeft: ''
+    instr: First<TBfCode>
+    instrRight: RemoveFirst<TBfCode>
+    memLeft: ''
+    mem: '\u0000'
+    memRight: EmptyMem
+    stdin: TStdin
+    stdout: ''
+}
+
+export type ProgramSummary<T extends CompletedProgram> = {
+    program: `${T['instrLeft']}${T['instr']}${T['instrRight']}`
+    memory: `${T['memLeft']}${T['mem']}${T['memRight']}`
+    setMemory: StripNullChars<`${T['memLeft']}${T['mem']}${T['memRight']}`>
+    stdout: T['stdout']
 }
 
 export type Next<T extends Program> =
@@ -38,6 +67,9 @@ export type Next<T extends Program> =
         memLeft: `${T['memLeft']}${T['mem']}`
         mem: First<T['memRight']>
         memRight: RemoveFirst<T['memRight']>
+
+        stdin: T['stdin']
+        stdout: T['stdout']
     }
     : T['instr'] extends '<' ? {
         halt: T['instrRight'] extends '' ? true : false
@@ -49,6 +81,9 @@ export type Next<T extends Program> =
         memLeft: RemoveLast<T['memLeft']>
         mem: Last<T['memLeft']>
         memRight: `${T['mem']}${T['memRight']}`
+
+        stdin: T['stdin']
+        stdout: T['stdout']
     }
     : T['instr'] extends '+' ? {
         halt: T['instrRight'] extends '' ? true : false
@@ -60,6 +95,9 @@ export type Next<T extends Program> =
         memLeft: T['memLeft']
         mem: T['mem'] extends MemByte ? Increment<T['mem']> : ''
         memRight: T['memRight']
+
+        stdin: T['stdin']
+        stdout: T['stdout']
     }
     : T['instr'] extends '-' ? {
         halt: T['instrRight'] extends '' ? true : false
@@ -71,31 +109,11 @@ export type Next<T extends Program> =
         memLeft: T['memLeft']
         mem: T['mem'] extends MemByte ? Decrement<T['mem']> : ''
         memRight: T['memRight']
+
+        stdin: T['stdin']
+        stdout: T['stdout']
     }
     : null
 
-export type BfProgram<
-    TBfCode extends string
-> = {
-    halt: false
-    instrLeft: ''
-    instr: First<TBfCode>
-    instrRight: RemoveFirst<TBfCode>
-    memLeft: ''
-    mem: '\u0000'
-    memRight: EmptyMem
-}
-
 export type RunToEnd<T extends Program> = T['halt'] extends true
     ? T : RunToEnd<Next<T>>
-
-
-interface CompletedProgram extends Program {
-    halt: true
-}
-
-export type ProgramSummary<T extends CompletedProgram> = {
-    program: `${T['instrLeft']}${T['instr']}${T['instrRight']}`
-    memory: `${T['memLeft']}${T['mem']}${T['memRight']}`
-    setMemory: StripNullChars<`${T['memLeft']}${T['mem']}${T['memRight']}`>
-}
