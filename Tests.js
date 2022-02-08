720ab7f9d839eabe710f16f09b849cfc49420451
Neil Bostian <neil.bostian@gmail.com>
2022-02-08 04:35:46 -0500

    feat: delete *.js

--- Tests.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var result1 = '\u0001\u0002\u0003';
-var result2A = 'cab'; // order bytes are printed to stdout is 'cab'
-var result2B = 'abc'; // order bytes are read into memory is 'abc'
